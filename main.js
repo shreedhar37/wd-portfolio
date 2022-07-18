4be13c83b4d9e7dcfd9a3e185642db0894d79f39
Shreedhar Chavan <meshree4@gmail.com>
2022-07-18 11:46:25 +0530

    Password field removed, message field added.

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ if (userEntries) {
 const saveUserForm = (event) => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
   const dob = document.getElementById("dob").value;
+  const message = document.getElementById("message").value;
   const acceptTermsAndConditions =
     document.getElementById("acceptTerms").checked;
 
@@ -21,14 +21,6 @@ const saveUserForm = (event) => {
   else acceptTnc = "not accepted";
 
   let errors = [];
-
-  if (password.length < 8) {
-    errors.push("Password must be atleast 8 characters long !!!");
-  }
-
-  if (password.length > 16) {
-    errors.push("Password can't be longer than 16 characters long !!");
-  }
 
   let currentYear = parseInt(new Date().getFullYear());
   let userDobYear = parseInt(dob.slice(0, 4));
@@ -42,9 +34,9 @@ const saveUserForm = (event) => {
     const userDetails = {
       name,
       email,
-      password,
       dob,
       acceptTnc,
+      message,
     };
     userEntries.push(userDetails);
     localStorage.setItem("user-entries", JSON.stringify(userEntries));
@@ -70,10 +62,11 @@ const displayEntries = () => {
       .map((entry) => {
         const name = `<td>${entry.name}</td>`;
         const email = `<td>${entry.email}</td>`;
-        const password = `<td>${entry.password}</td>`;
         const dob = `<td>${entry.dob}</td>`;
         const acceptTerms = `<td>${entry.acceptTnc}</td>`;
-        const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
+        const message = `<td>${entry.message}</td>`;
+
+        const row = `<tr>${name} ${email}  ${dob} ${acceptTerms} ${message}</tr>`;
         return row;
       })
       .join("\n");
@@ -81,9 +74,9 @@ const displayEntries = () => {
   var table = `<table border='1' width='100%'><tr>
       <th>Name</th>
       <th>Email</th>
-      <th>Password</th>
       <th>dob</th>
       <th>accepted terms?</th>
+      <th>Message</th>
     </tr>${entries} </table>`;
   let details = document.getElementById("user-entries");
   details.innerHTML = table;
